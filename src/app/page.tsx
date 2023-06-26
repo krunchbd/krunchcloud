5e614a6c388322c34afc91a848157d60a68f0f78
arkochan <najmussakib@iut-dhaka.edu>
2023-06-26 17:33:03 +0600

    next auth ektu baki ase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { Montserrat } from "next/font/google";
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/server/auth";
+import { unknown } from "zod";
+import {
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "~/components/buttons.component";
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({
@@ -8,12 +17,6 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-const Home = () => {
-  return (
-    <div className="h-screen">
-      <p className="text-white">Cool</p>
-    </div>
-  );
-};
-
-export default Home;
+export default async function page() {
+  return <div className="text-white"></div>;
+}
